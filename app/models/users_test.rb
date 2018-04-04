1a71f2fb2eab47f65dc8d16175a712ae911c5790
alvi2496 <alvi.utsab@gmail.com>
2018-04-04 22:21:45 +0600

    accessor update

--- app/models/users_test.rb
+++ app/models/users_test.rb
@@ -1,4 +1,15 @@
 class UsersTest < ApplicationRecord
   belongs_to :user, inverse_of: :users_tests
   belongs_to :test, inverse_of: :users_tests
+
+  after_create :assign_accessor
+
+  enum accessor: %i[teacher student]
+
+  private
+
+  def assign_accessor
+    self.accessor = user.teacher? ? 'teacher' : 'student'
+    save
+  end
 end
